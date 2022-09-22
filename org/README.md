ddaa469c051f51187e63631b5b0d9f63b96e6294
Sachit Dabas <sachitdabas@MacBook-Pro.local>
2022-09-22 15:20:01 -0700

    draft

---
+++ org/README.md
@@ -0,0 +1,56 @@
+# org
+
+This smart contract allows an org to produce and to consume produced badges.
+
+
+## Producers and Consumers
+
+Early dive into these two broad categories makes for easier understanding.
+
+### Producer Badge Contracts
+Producers Badge Contracts produce the badges. 2 Producer Contracts are available -
+1) Simple Badge Contract - Creates a badge, which a person of authority in an org can issue to  members of org.
+2) Gotcha Badge Contact - Creates a badge which members of org can issue to other members. A gotcha badge comes with Supply Cap for a Time Duration Cycle. Supply Cap is restored at end of every Time Duration Cycle.
+
+### Consumer Badge Contracts
+
+3 Consumers Contracts are  available -
+
+1) Atomic Assets Consumer - This consumes badges at granular level in atomic asset standard. i.e. for every Badge a non transferrable asset is created in atomic assets standard. 
+2) Cumulative Consumer - This consumers badges at cumulative level. Same badges are added and stored as one record.
+3) Rounds Consumer - This consumer accumulate badges and their corresponding scores from the start to the end of the round. Start and End of round are triggered by calling actions.
+
+## Usage
+
+For org to use above features, it need to -
+### 1 -  Deploy org smart contract and validation contract.
+Deploy this contract and a validation contract similar to Eden Validation Contract.
+
+### 2 - Initialize org contract
+
+Org would need to do some basic setup, which involves
+-Subscribing to the producer contracts.
+-And Subscribe to a validation contract. Org needs to implement this to validate the input of actions. See Eden Validation Contract.
+-If an org would want to subscribe to Atomic Asset as consumer for the badges, it would need to call this action.
+
+All this can be done via this action -
+<ACTION initsystem>
+
+### 3 - Badge definition 
+With initial setup out of the way, next an org can define some simple badges and gotcha badge.
+<ACTION initsimple>
+<ACTION initgotcha>
+
+
+### 4 - Badge Issuance
+With badge creation out of the way, members and issuing authority can start issue badges via actions.
+<ACTION givesimple>
+<ACTION givegotcha>
+
+### 5 - Other details about rounds consumer
+Addtional info about rounds consumer contract - 
+
+To utilize features of Rounds Consumer, org needs to create a round, add badges and their corresponding scores and start the round. A round can be ended by calling endround action.
+
+Account that receives the badges, need to opt in to receive badges from an org. This can be done via action -
+
---
+++ org/src/org.cpp
@@ -0,0 +1,266 @@
+#include <org.hpp>
+
+// todo
+// 1) remove check for init missing in settings for checks contract.
+
+  ACTION org::initsystem(name checks_contract, vector<name> badges_types, name aacollection) {
+    require_auth (org);
+    action {
+      permission_level{get_self(), name("active")},
+      name(get_self()),
+      name("chkscontract"),
+      chkscontract_args {
+        .checks_contract = checks_contract}
+    }.send();
+
+
+    for(auto i = 0; i < badges_types.size(); i++) {
+      action {
+      permission_level{get_self(), name("active")},
+      name(ORCHESTRATOR_CONTRACT),
+      name("recognize"),
+      orchestrator_recognize_args {
+        .org = get_self(),
+        .trusted_badge_contract = badges_types[i]}
+      }.send();
+    }
+
+    if(aacollection.to_string().size>0) {
+      action {
+      permission_level{get_self(), name("active")},
+      name(AA_BADGE_CONTRACT),
+      name("initcoll"),
+      aa_initcoll_args {
+        .org = get_self(),
+        .collection_name = aacollection}
+      }.send();
+    }
+  }
+
+  ACTION org::chkscontract (name checks_contract) {
+    require_auth(get_self());
+    settings_table _settings( get_self(), get_self().value );
+    auto itr = _settings.find(1);
+    if(itr == _settings.end()) {
+      _settings.emplace(get_self(), [&](auto& row) {
+        row.id = 1;
+        row.checks_contract = checks_contract;
+      });
+    } else {
+      _settings.modify(itr, get_self(),[&](auto& row) {
+        row.checks_contract = checks_contract;
+      });
+    }
+  }
+
+  ACTION org::initsimple (name creator, name badge, string ipfs_image, string display_name, vector<name> features) {
+    require_auth(creator);
+
+    require_recipient(checkscontract());
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(SIMPLEBADGE_CONTRACT),
+      name("create"),
+      createsimple_args {
+        .org = get_self(),
+        .badge = badge,
+        .parent_badges = [],
+        .ipfs_image = ipfs_image,
+        .memo = display_name}
+    }.send();
+
+    for (auto i = 0 ; i < features.size(); i++) {
+      action {
+      permission_level{get_self(), name("active")},
+      name(ORCHESTRATOR_CONTRACT),
+      name("addfeature"),
+      createsimple_args {
+        .org = get_self(),
+        .badge_contract = name(SIMPLEBADGE_CONTRACT),
+        .badge_name = badge_name,
+        .notify_account = features[i],
+        .memo = display_name}
+      }.send();
+    }
+  }
+
+  ACTION org::initgotcha (name creator, 
+    name badge, 
+    time_point_sec starttime, 
+    uint64_t cycle_length, 
+    uint8_t supply_per_cycle, 
+    string ipfs_image, 
+    string display_name, 
+    vector<name> features) {
+
+    require_auth(creator);
+    
+    require_recipient(checkscontract());
+    
+    action {
+      permission_level{get_self(), name("active")},
+      name("GOTCHABADGE_CONTRACT"),
+      name("create"),
+      creategotcha_args {
+        .org = get_self(),
+        .badge = badge,
+        .starttime = starttime,
+        .cycle_length = cycle_length,
+        .supply_per_cycle = supply_per_cycle,
+        .ipfsimage = ipfs_image,
+        .memo = display_name }
+    }.send();
+
+    for (auto i = 0 ; i < features.size(); i++) {
+      action {
+      permission_level{get_self(), name("active")},
+      name(ORCHESTRATOR_CONTRACT),
+      name("addfeature"),
+      orchestrator_addfeature_args {
+        .org = get_self(),
+        .badge_contract = name(SIMPLEBADGE_CONTRACT),
+        .badge_name = badge_name,
+        .notify_account = features[i],
+        .memo = display_name}
+      }.send();
+    }
+  }
+
+  ACTION org::givegotcha (name badge, name from, name to, uint8_t amount, string memo ) {
+    require_auth(from);
+    
+    require_recipient(checkscontract());
+
+    action {
+      permission_level{get_self(), name("active")},
+      name("gotchabadge"),
+      name("give"),
+      givegotcha_args {
+        .org = get_self(),
+        .badge = badge,
+        .from = from,
+        .to = to,
+        .amount = amount,
+        .memo = memo }
+    }.send();
+
+  }
+
+  ACTION org::givesimple (name authorizer, name to, name badge, string memo ) {
+    require_auth(authorizer);
+
+    require_recipient(checkscontract());
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(SIMPLEBADGE_CONTRACT),
+      name("give"),
+      givesimple_args {
+        .org = get_self(),
+        .to = to,
+        .badge = badge,
+        .memo = memo }
+    }.send();
+  }
+
+  ACTION org::initround (name authorizer, name round, string display_name) {
+    require_auth(authorizer);
+    require_recipient(checkscontract());
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(ROUNDS_CONTRACT),
+      name("createround"),
+      createround_args {
+        .org = get_self(),
+        .round = round,
+        .description = display_name,
+        .account_constrained = false,
+        .participating_accounts = []}
+    }.send();
+  }
+
+  ACTION org::startround (name authorizer, name round) {
+    require_auth(authorizer);
+    require_recipient(checkscontract());
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(ROUNDS_CONTRACT),
+      name("startround"),
+      startround_args {
+        .org = get_self(),
+        .round = round}
+    }.send();
+  }
+  
+  ACTION org::endround (name authorizer, name round) {
+    require_auth(authorizer);
+    require_recipient(checkscontract());
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(ROUNDS_CONTRACT),
+      name("endround"),
+      endround_args {
+        .org = get_self(),
+        .round = round}
+    }.send();
+  }
+
+  ACTION org::addbdgetornd (name authorizer,
+    name round, 
+    uint64_t badge_id, 
+    name balance_based_scoring_type, 
+    uint16_t balance_based_scoring_weight,
+    name source_based_scoring_type,
+    uint16_t source_based_scoring_weight) {
+    
+    require_auth(authorizer);
+    require_recipient(checkscontract());  
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(ROUNDS_CONTRACT),
+      name("addscoremeta"),
+      addscoremeta_args {
+        .org = get_self(),
+        .round = round,
+        .badge_id = badge_id,
+        .balance_based_scoring_type = balance_based_scoring_type,
+        .balance_based_scoring_weight =balance_based_scoring_weight,
+        .source_based_scoring_type = source_based_scoring_type,
+        .source_based_scoring_weight = source_based_scoring_weight
+        }
+    }.send();
+  }
+
+  ACTION org::accntoptin(name account) {
+    require_auth(account);
+    require_recipient(checkscontract()); 
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(ACCOUNT_PREFERENCES_CONTRACT),
+      name("allow"),
+      accountprefs_allow_args {
+        .account = account,
+        .org = get_self()}
+    }.send(); 
+  }
+  
+  ACTION org::accntoptout(name account, string memo) {
+    require_auth(account);
+    require_recipient(checkscontract());  
+
+    action {
+      permission_level{get_self(), name("active")},
+      name(ACCOUNT_PREFERENCES_CONTRACT),
+      name("unallow"),
+      accountprefs_unallow_args {
+        .account = account,
+        .org = get_self(),
+        .memo = memo}
+    }.send();
+  }
