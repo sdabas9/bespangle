27a1c07466f40af86a286ce6b93fb66671b7081c
Sachit Dabas <sachitdabasofficial@gmail.com>
2023-01-01 17:07:55 -0800

    temp

---
+++ tap/src/tap.cpp
@@ -0,0 +1,17 @@
+#include <tap.hpp>
+
+ACTION tap::pause(uint64_t badgeid) {
+
+}
+
+ACTION tap::resume(uint64_t badgeid) {
+
+}
+
+ACTION tap::duration(uint64_t starttime, uint64_t endtime) {
+
+}
+
+ACTION tap::maxcap(uint64_t maxcap) {
+
+}
